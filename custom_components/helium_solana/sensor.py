c8ecb4798d9f33b0ba0dad26578ffea9c846c5f9
Nathan Spencer <natekspencer@gmail.com>
2023-08-31 20:29:23 +0000

    Utilize a data coordinator for helium stats

--- custom_components/helium_solana/sensor.py
+++ custom_components/helium_solana/sensor.py
@@ -29,6 +29,7 @@ from homeassistant.helpers.device_registry import DeviceEntryType
 from .utility import http_client
 
 from .api.backend import BackendAPI
+from .coordinator import HeliumSolanaDataUpdateCoordinator
 from .sensors.WalletBalance import WalletBalance
 from .sensors.HotspotReward import HotspotReward
 from .sensors.HeliumStats import HeliumStats
@@ -75,7 +76,7 @@ async def async_setup_entry(
     config = hass.data[DOMAIN][config_entry.entry_id]
     integration = config.get(CONF_INTEGRATION)
     wallet = config.get(CONF_WALLET)
-    sensors = await get_sensors(integration, wallet, None)
+    sensors = await get_sensors(integration, wallet, None, hass)
     
     #version = config.get(CONF_VERSION)
     #if version == 2:
@@ -101,7 +102,7 @@ async def async_setup_platform(
 
     async_add_entities(sensors, update_before_add=True)
 
-async def get_sensors(integration, wallet, prices):
+async def get_sensors(integration, wallet, prices, hass):
     sensors = []
 
     if integration == 'general_token_price':
@@ -115,17 +116,20 @@ async def get_sensors(integration, wallet, prices):
                 sensors.append(PriceSensor(price))
 
     if integration == 'general_stats':
-        sensors.append(HeliumStats(api_backend, 'IOT', 'total_hotspots', 'Total Hotspots', ['stats', 'iot', 'total_hotspots'], 'mdi:router-wireless', 'Hotspots'))
-        sensors.append(HeliumStats(api_backend, 'IOT', 'active_hotspots', 'Active Hotspots',  ['stats', 'iot', 'active_hotspots'], 'mdi:router-wireless', 'Hotspots'))
-        sensors.append(HeliumStats(api_backend, 'IOT', 'total_cities', 'Total Cities', ['stats', 'iot', 'total_cities'],'mdi:city', 'Cities'))
-        sensors.append(HeliumStats(api_backend, 'IOT', 'total_countries', 'Total Countries', ['stats', 'iot', 'total_countries'], 'mdi:earth', 'Countries'))
-        sensors.append(HeliumStats(api_backend, 'IOT', 'daily_average_rewards', 'Daily Average Rewards', ['stats', 'iot', 'daily_average_rewards'], 'mdi:hand-coin-outline', 'IOT', 'float'))
+        coordinator = HeliumSolanaDataUpdateCoordinator(hass, api_backend)
+        await coordinator.async_config_entry_first_refresh()
 
-        sensors.append(HeliumStats(api_backend, 'MOBILE', 'total_hotspots', 'Total Hotspots', ['stats', 'mobile', 'total_hotspots'],'mdi:router-wireless', 'Hotspots'))
-        sensors.append(HeliumStats(api_backend, 'MOBILE', 'active_hotspots', 'Active Hotspots', ['stats', 'mobile', 'active_hotspots'],'mdi:router-wireless', 'Hotspots'))
-        sensors.append(HeliumStats(api_backend, 'MOBILE', 'total_cities', 'Total Cities', ['stats', 'mobile', 'total_cities'], 'mdi:city', 'Cities'))
-        sensors.append(HeliumStats(api_backend, 'MOBILE', 'total_countries', 'Total Countries', ['stats', 'mobile', 'total_countries'], 'mdi:earth', 'Countries'))
-        sensors.append(HeliumStats(api_backend, 'MOBILE', 'daily_average_rewards', 'Daily Average Rewards', ['stats', 'mobile', 'daily_average_rewards'], 'mdi:hand-coin-outline', 'MOBILE' ,'float'))
+        sensors.append(HeliumStats(coordinator, 'IOT', 'total_hotspots', 'Total Hotspots', ['stats', 'iot', 'total_hotspots'], 'mdi:router-wireless'))
+        sensors.append(HeliumStats(coordinator, 'IOT', 'active_hotspots', 'Active Hotspots',  ['stats', 'iot', 'active_hotspots'], 'mdi:router-wireless'))
+        sensors.append(HeliumStats(coordinator, 'IOT', 'total_cities', 'Total Cities', ['stats', 'iot', 'total_cities'],'mdi:city'))
+        sensors.append(HeliumStats(coordinator, 'IOT', 'total_countries', 'Total Countries', ['stats', 'iot', 'total_countries'], 'mdi:earth'))
+        sensors.append(HeliumStats(coordinator, 'IOT', 'daily_average_rewards', 'Daily Average Rewards', ['stats', 'iot', 'daily_average_rewards'], 'mdi:hand-coin-outline', 'IOT', 'float'))
+
+        sensors.append(HeliumStats(coordinator, 'MOBILE', 'total_hotspots', 'Total Hotspots', ['stats', 'mobile', 'total_hotspots'],'mdi:router-wireless'))
+        sensors.append(HeliumStats(coordinator, 'MOBILE', 'active_hotspots', 'Active Hotspots', ['stats', 'mobile', 'active_hotspots'],'mdi:router-wireless'))
+        sensors.append(HeliumStats(coordinator, 'MOBILE', 'total_cities', 'Total Cities', ['stats', 'mobile', 'total_cities'], 'mdi:city'))
+        sensors.append(HeliumStats(coordinator, 'MOBILE', 'total_countries', 'Total Countries', ['stats', 'mobile', 'total_countries'], 'mdi:earth'))
+        sensors.append(HeliumStats(coordinator, 'MOBILE', 'daily_average_rewards', 'Daily Average Rewards', ['stats', 'mobile', 'daily_average_rewards'], 'mdi:hand-coin-outline', 'MOBILE' ,'float'))
 
     if integration == 'wallet':
         
--- custom_components/helium_solana/sensors/HeliumStats.py
+++ custom_components/helium_solana/sensors/HeliumStats.py
@@ -1,98 +1,85 @@
+"""Helium stats entity."""
+from __future__ import annotations
 
-from typing import Any, Callable, Dict, Optional
-from homeassistant.helpers.entity import (
-    Entity,
-    DeviceInfo
-)
-import requests
-from ..const import (
-    DOMAIN
-)
-
+from datetime import date, datetime
+from decimal import Decimal
 import logging
+from typing import Any, Coroutine
+
+from homeassistant.components.sensor import SensorEntity, SensorStateClass
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers.device_registry import (
+    CONNECTION_BLUETOOTH,
+    CONNECTION_NETWORK_MAC,
+    format_mac,
+)
+from homeassistant.helpers.entity import DeviceInfo, EntityDescription
+from homeassistant.helpers.typing import EventType, StateType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+import requests
+
+from ..api.backend import BackendAPI
+from ..const import DOMAIN
+from ..coordinator import HeliumSolanaDataUpdateCoordinator
+
 _LOGGER = logging.getLogger(__name__)
 
 
-class HeliumStats(Entity):
-    """Helium Stats"""
-    def __init__(self, api, token, key, name, path, icon, uom, type='int'):
-        super().__init__()
-        self.api = api
+class HeliumStats(CoordinatorEntity[HeliumSolanaDataUpdateCoordinator], SensorEntity):
+    """Helium stats sensor entity."""
+
+    _attr_should_poll = True
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def __init__(
+        self,
+        coordinator: HeliumSolanaDataUpdateCoordinator,
+        token: str,
+        key: str,
+        name: str,
+        path: list[str],
+        icon: str,
+        uom: str | None = None,
+        _type: str = "int",
+    ) -> None:
+        """Initialize the entity."""
+        super().__init__(coordinator)
         self.token = token
         self.key = key
         self.path = path
-        self._available = True
-        self._icon = icon
-        self._unique_id = 'helium.stats.'+token+'_'+key.lower()
-        self.device_unique_id = 'helium.stats.'+token
-        self._name = 'Helium Stats '+token+' '+name
-        self.uom = uom
-        self.type = type
+        self.device_unique_id = "helium.stats." + token
+        self.type = _type
 
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def unique_id(self) -> str:
-        return self._unique_id
-
-    @property
-    def available(self) -> bool:
-        return self._available
-
-    @property
-    def state(self) -> Optional[str]:
-        return self._state
-
-    @property
-    def icon(self) -> str | None:
-        return self._icon
-    
-    @property
-    def should_poll(self):
-        return True
-
-    @property
-    def unit_of_measurement(self):
-        return self.uom
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return the device info."""
-        return DeviceInfo(
+        # self._attr_available = True
+        self._attr_device_info = DeviceInfo(
             identifiers={
                 # Serial numbers are unique identifiers within a specific domain
                 (DOMAIN, self.device_unique_id)
             },
-            name='Helium Stats '+self.token,
-            #node_name=self._name,
-            manufacturer='Helium'
+            name="Helium Stats " + self.token,
+            manufacturer="Helium",
         )
+        self._attr_icon = icon
+        self._attr_name = "Helium Stats " + token + " " + name
+        self._attr_native_unit_of_measurement = uom
+        self._attr_unique_id = "helium.stats." + token + "_" + key.lower()
 
-    
-    async def async_update(self):
-        try:
+        self._set_native_value()
 
-            
-            response = await self.api.get_data('heliumstats')
-
-            if response.status_code != 200:
-                return
-            
-            value = response.json()
+    def _set_native_value(self) -> None:
+        if value := self.coordinator.data:
             for key in self.path:
                 value = value[key]
-
-            if self.type == 'str':
+            if self.type == "str":
                 value = str(value)
-            elif self.type == 'float':
+            elif self.type == "float":
                 value = float(value)
             else:
                 value = int(value)
-            self._state = value
-            self._available = True
+        self._attr_native_value = value
 
-        except (requests.exceptions.RequestException):
-            self._available = False
-            _LOGGER.exception("Error retrieving helium stats from hotspotty")
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._set_native_value()
+        return super()._handle_coordinator_update()
